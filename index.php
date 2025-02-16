ec3098c99847cd4c35de467d9562f4fcf3043168
GRANADRI <adrianmv25@outlook.es>
2025-02-16 18:32:27 +0100

    idk

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Charcuter&iacute;a</title>
+</head>
+<body>
+    <header id="header">
+        <div id="logo">
+            
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <aside>
+
+    </aside>
+    <footer>
+
+    </footer>
+</body>
+</html>
