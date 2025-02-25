9f286662125bd53cfc919978e540f263c9175b2c
GRANADRI <adrianmv25@outlook.es>
2025-02-25 14:27:42 +0100

    commit estilos repasados y comentarios del anterior commit que se me olvido decir en el anterior commit

--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -1,24 +1,26 @@
-    <div id="contenido">
-        <!-- Barra lateral -->
-        <aside id="sidebar">
-            <h3>Iniciar Sesi&oacute;n / Registrarse</h3>
-            <div id="login" class="block_aside">
-                <form action="#" method="POST">
-                    <label for="email">Correo</label>
-                    <input type="email" name="email">
-                    <label for="password">Contrase&ntilde;a</label>
-                    <input type="password" name="password">
-                    <input type="submit" value="Enviar">
-                </form>
+<div id="contenido">
+    <!-- Barra lateral -->
+    <aside id="sidebar">
+        <h3>Iniciar Sesi&oacute;n / Registrarse</h3>
+        <div id="login" class="block_aside">
+            <form action="#" method="POST">
+                <label for="email">Correo</label>
+                <input type="email" name="email">
+                <label for="password">Contrase&ntilde;a</label>
+                <input type="password" name="password">
+                <input type="submit" value="Enviar">
+            </form>
 
-                <a href="#">Mis pedidos</a>
-                <a href="#">Gestionar pedidos</a>
-                <a href="#">Gestionar categor&iacute;as</a>
-            </div>
-        </aside>
+            <a href="#">Mis pedidos</a>
+            <a href="#">Gestionar pedidos</a>
+            <a href="#">Gestionar categor&iacute;as</a>
+        </div>
+    </aside>
 
-        <!-- Contenido principal -->
-        <main>
+    <!-- Contenido principal -->
+    <main>
+        <h1>Productos destacados</h1>
+        <div class="productos">
             <div class="producto">
                 <img src="<?=BASE_URL?>assets/images/jamonpatanegra.png">
                 <h2>Jam&oacute;n Pata Negra 50% Ib&eacute;rica</h2>
@@ -39,5 +41,6 @@
                 <p>150€</p>
                 <a href="">Comprar</a>
             </div>
-        </main>
-    </div>
+        </div>
+    </main>
+</div>
